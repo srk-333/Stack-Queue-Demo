6f8bb493669eb4b5d36534e8da7bacb7124e639a
srk-333 <sauravmk333@gmail.com>
2022-01-27 22:45:19 +0530

    [Saurav]Add:Created a Stack Using Linked List

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StackAndQueue.Stack;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,47 @@ using System.Threading.Tasks;
 
 namespace StackAndQueue
 {
-     class Program
+    class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue program using Linked List");
+            CallStackQueue();
             Console.ReadLine();
+        }
+        /// <summary>
+        /// Calls the stack queue.
+        /// </summary>
+        public static void CallStackQueue()
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            bool CONTINUE = true;
+            while (CONTINUE)
+            {
+                Console.WriteLine("\nEnter your option!");
+                Console.WriteLine("1=Stack , 2=Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("<---Creating A Stack--->");
+                        linkedListStack.Push(70);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(56);
+                        linkedListStack.Display();
+                        break;                
+                    case 2:
+                        CONTINUE = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option!");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ StackAndQueue/Stack/LinkedListStack.cs
@@ -0,0 +1,43 @@
+﻿using StackAndQueue.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue.Stack
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        /// <summary>
+        /// add element into stack.
+        /// </summary>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = this.top;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        /// <summary>
+        /// display the stack elements.
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine("Data in Stack :" +temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
