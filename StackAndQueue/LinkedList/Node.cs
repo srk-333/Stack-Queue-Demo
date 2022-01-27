6108a3a2a81bc8cc54074939d84befa0e6a72f69
srk-333 <sauravmk333@gmail.com>
2022-01-27 22:21:28 +0530

    [Saurav]Add:Welcome Program and Linked List Node

---
+++ StackAndQueue/LinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue.LinkedList
+{
+    public class Node
+    {
+        // variables
+        public int data;
+        public Node next;
+        /// <summary>
+        /// constructor is used to initialized the data.
+        /// </summary>
+        /// <param name="data"></param>
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue program using Linked List");
+            Console.ReadLine();
+        }
+    }
+}
